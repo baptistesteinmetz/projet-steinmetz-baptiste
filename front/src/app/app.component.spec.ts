d80ceb1aa776dcff2143f80916fe95b76c464b52
baptiste.steinmetz <baptiste.steinmetz67@gmail.com>
2021-01-08 18:01:34 +0100

    test heroku setup

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tp02'`, () => {
+  it(`should have as title 'store-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tp02');
+    expect(app.title).toEqual('store-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tp02 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('store-web app is running!');
   });
 });
